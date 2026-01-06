4e085a45177d30f46b991a27f64382b63ed74ccb
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2026-01-06 19:54:37 +0800

    added routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Game from "./pages/Game";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Leaderboard from "./pages/Leaderboard";
+
 function App() {
-	return <></>;
+	return (
+		<div>
+			<Routes>
+				{/* TODO: Game is different how to know which is which smth */}
+				<Route path="/" element={<Game />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/leaderboards" element={<Leaderboard />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
