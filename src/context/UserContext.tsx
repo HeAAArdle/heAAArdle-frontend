25217d872f0b60a473dc456d329d643c90387f0c
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2026-01-13 20:04:06 +0800

    slight changes

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,23 +1,16 @@
 import { createContext } from "react";
 import type { ReactNode } from "react";
-import { useSongs } from "../services/queries/songs";
 
-type UserContextType = {};
+type UserContextType = {
+	noOfGuesses: number;
+};
 
-export const UserContext = createContext<UserContextType | undefined>(
-	undefined
-);
+export const UserContext = createContext<UserContextType | null>(null);
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
 	const noOfGuesses = 6;
 
-	const {
-		data: SONGS,
-		isLoading: isSongsLoading,
-		error: songsError,
-	} = useSongs();
-
-	const value = { SONGS, isSongsLoading, songsError, noOfGuesses };
+	const value = { noOfGuesses };
 
 	return (
 		<UserContext.Provider value={value}>{children}</UserContext.Provider>
